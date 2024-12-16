7fe8a9821dc166dfe52e9766bb1ce5adce8596be
Luciano Alfredo <lucianalfred60@gmail.com>
2024-12-16 15:59:18 +0100

    test

--- almost_prime.cpp
+++ almost_prime.cpp
@@ -3,46 +3,52 @@
 
 using namespace std;
 
-int countAlmostPrimes(int n){
-
-    vector<bool> sieve(n+1, true);
-    sieve[0] = sieve[1]  = false;
-
-    for(int i = 2; i * i <= n; i++){
-        if(sieve[i]){
-            for(int j = i * i; j <= n; j += i){
-                sieve[j] = false;
-            }
-        }
-    }
-
-    vector<int> primes;
-    for(int i = 2; i <= n; i++){
-        if(sieve[i]){
-            primes.push_back(i);
-        }
-    }
-
-    int count = 0;
-    int total_primes = primes.size();
-
-    for(int i = 0; i < total_primes; i++){
-        for(int j = i + 1; j < total_primes; j++){
-            int product = primes[i] * primes[j];
-            if(product <= n){
-                count++;
-            }else{
-                break;
-            }
-        }
-    }
-
-    return count;
-}
-
 int main(){
     int n;
+
     cin >> n;
-    cout << countAlmostPrimes(n) << endl;
+
+    vector<bool> is_prime(n + 1, true); 
+    is_prime[0] = is_prime[1] = false;
+
+    for(int i = 2; i * i <= n; ++i){
+        if(is_prime[i]){
+            for(int j = i * i; j <= n; j += i){
+                is_prime[j] = false;
+            }
+        }
+    }
+
+    int almost_prime_count = 0;
+    for (int i = 2; i <= n; i++){
+        if (is_prime[i]){
+            continue;
+        }
+        int num_prime_divisors = 0;
+        int x = i;
+
+        for(int j = 2; j * j <= x; ++j){
+            if (x % j == 0 && is_prime[j]){
+                num_prime_divisors++;
+
+                while ( x % j == 0){
+                    x /= j;
+                }
+            }
+        }
+
+        if ( x > 1 && is_prime[x]){
+            num_prime_divisors++;
+        }
+
+        if (num_prime_divisors == 2){
+            almost_prime_count++;
+        }
+
+
+    }
+
+    cout << almost_prime_count << endl;
+
     return 0;
 }
