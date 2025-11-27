95cccaadeb92dce68974d993c6c58d180765f79e
Luciano Alfredo <lalfredo@c2r5s4.42luanda.com>
2025-11-27 19:26:50 +0100

    trainning

--- trainning/A/shaass_and_oskols.py
+++ trainning/A/shaass_and_oskols.py
@@ -1,16 +1,24 @@
 list_size =  int(input())
-
-birds_list_per_wire = []
-input_str = input().strip().split()
-for i in range(list_size):
-    birds_list_per_wire.append(int(input_str[i]))
-
+birds_list_per_wire = list(map(int, input().split()))
 total_shots = int(input())
-tests = 0
 
-while tests < total_shots:
-    input_str = input().strip().split()
-    wire = int(input_str[0])
-    bird = int(input_str[1])
+
+for _ in  range(total_shots):
+    wire, bird = map(int, input().split())
+    wire_index = wire - 1
+
+    birds_in_wire = birds_list_per_wire[wire_index]
+    left_birds = bird - 1
+    right_birds = birds_in_wire - bird
+
+    if wire_index - 1 >= 0:
+        birds_list_per_wire[wire_index - 1] += left_birds
     
-    tests = tests + 1
+    if wire_index + 1 < list_size:
+        birds_list_per_wire[wire_index + 1] += right_birds
+    birds_list_per_wire[wire_index] = 0
+
+    total_shots -= 1
+
+for birds in birds_list_per_wire:
+    print(birds)
